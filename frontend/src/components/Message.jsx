e08777d5308b94dc5c50ce6bb20d7f9110ba6b3e
UTKARSH331 <129758419+UTKARSH331@users.noreply.github.com>
2026-02-19 13:56:12 +0530

    Update Message.jsx

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { BsCheckAll } from "react-icons/bs";
