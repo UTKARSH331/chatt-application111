910def3b44df6261f7f5792bbb889d47d7e338df
UTKARSH331 <129758419+UTKARSH331@users.noreply.github.com>
2026-03-17 23:15:37 +0530

    Delete antigravity-core directory

--- antigravity-core/index.js
+++
@@ -1,9 +0,0 @@
-const AntigravityNode = require('./src/network/connection_manager');
-const AntigravityClient = require('./src/api/client');
-const WebSocketBridge = require('./src/bridge/ws_bridge');
-
-module.exports = {
-    AntigravityNode,
-    AntigravityClient,
-    WebSocketBridge
-};
